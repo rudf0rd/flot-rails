c22a8b0a70833f071b41efe9b55fadf1c5fe26e1
Holger Arndt <arndt@comcard.de>
2013-01-04 11:46:18 +0100

    Small optical adjustment

--- lib/flot-rails/chart.rb
+++ lib/flot-rails/chart.rb
@@ -2,13 +2,14 @@ module Flot
   class InvalidDataset < RuntimeError; end
     
   BaseOpts = {
-    :bar => {bars: {show: true}, :prefix => :bar},
-    :line => {lines: {show: true}, :prefix => :line},
-    :point => {points: {show: true}, :prefix => :point},
-    :pie => {series: {pie: {show: true}}, :prefix => :pie}
+    :bar => {bars: {show: true}, prefix: :bar},
+    :line => {lines: {show: true}, prefix: :line},
+    :point => {points: {show: true}, prefix: :point},
+    :pie => {series: {pie: {show: true}}, prefix: :pie}
   };
   
   def chart(dataset, opts={})
+    readable_flag = opts.delete(:readable)
     uniq_name = [opts.delete(:prefix), :chart, dataset.join.hash.abs.to_s].compact.join('_')
     
     # dataset will be some kind of tripple nested arrays or an array of hashes with an 2-dimensional-array data-element
@@ -17,15 +18,15 @@ module Flot
       raise InvalidDataset unless ele.kind_of?(Array) or (ele.kind_of?(Hash) and ele.has_key?(:data))
     end
     
-    return (<<-HTML
+    return raw (<<-HTML
 <div class="inner" id="#{uniq_name}" style="width:600px;height:300px;"></div>
 <script type='text/javascript'>
   $(function () {
-    $.plot($(\"\##{uniq_name}\"), #{dataset.to_s.gsub(/:(\w*)=>/,'\1: ').gsub(/\s?,\s?/, ',')}, #{opts.to_s.gsub(/:(\w*)=>/, '\1: ')} ); 
+    $.plot($(\"\##{uniq_name}\"), #{dataset.to_s.gsub(/:(\w*)=>/, "\n" + '\1: ').gsub(/(\[|\{)(\[|\{)/, '\1' + "\n" + '\2  ').gsub(/],/, "],\n")}#{(', ' + opts.to_s.gsub(/:(\w*)=>/, "\n" + '\1: ')) unless opts.empty? } ); 
   });
 </script>
     HTML
-    ).html_safe    
+    ).html_safe
   end
     
   def self.chart_type(name)
